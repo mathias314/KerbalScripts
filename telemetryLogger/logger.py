e8446ca41ee336cc5ed06d6f1e9f3374c290a5fa
mathias314 <mathewclutter@gmail.com>
2020-12-15 01:08:44 -0700

    reorganizing repo, adding more plots, and adding file name input to logger

---
+++ telemetryLogger/logger.py
@@ -0,0 +1,59 @@
+import krpc
+import time
+import csv
+
+conn = krpc.connect(name = "logger")
+vessel = conn.space_center.active_vessel
+frame = vessel.orbit.body.reference_frame
+
+fileName = input("Enter csv file name (with .csv appended): ")
+
+f = open(fileName, "w")
+csvwriter = csv.writer(f)
+
+met = conn.add_stream(getattr, vessel, 'met')
+altitude = conn.add_stream(getattr, vessel.flight(), 'mean_altitude')
+latitude = conn.add_stream(getattr, vessel.flight(), 'latitude')
+longitude = conn.add_stream(getattr, vessel.flight(), 'longitude')
+atmosphereDensity = conn.add_stream(getattr, vessel.flight(), 'atmosphere_density')
+dynamicPressure = conn.add_stream(getattr, vessel.flight(), 'dynamic_pressure')
+totalAirTemp = conn.add_stream(getattr, vessel.flight(), 'total_air_temperature')
+staticAirTemp = conn.add_stream(getattr, vessel.flight(), 'static_air_temperature')
+
+fields = ['time', 'altitude', 'latitude', 'longitude', 'atmosphereDensity',
+'dynamicPressure', 'totalAirTemp', 'staticAirTemp', 'speed', 'acceleration', 'jerk', 'gForce']
+
+csvwriter.writerow(fields)
+
+speed = 0
+acceleration = 0
+jerk = 0
+gForce = 0
+
+i = 0
+
+while met() == 0:
+    pass
+
+row = [met(), altitude(), latitude(), longitude(), atmosphereDensity(), dynamicPressure(), totalAirTemp(), staticAirTemp(),
+speed, acceleration, jerk, gForce]
+
+while True:
+    oldSpeed = speed
+    oldAccel = acceleration
+    oldTime = met()
+
+    time.sleep(1)
+
+    speed = vessel.flight(frame).speed
+    acceleration = (speed - oldSpeed) / (met() - oldTime) #causes problems if the craft has slight movement on the pad
+    jerk = (acceleration - oldAccel) / (met() - oldTime) #have to add epsilon to prevent divide from 0 error when on pad
+    gForce = acceleration / 9.81
+
+    row = [met(), altitude(), latitude(), longitude(), atmosphereDensity(), dynamicPressure(), totalAirTemp(), staticAirTemp(),
+    speed, acceleration, jerk, gForce]
+
+    csvwriter.writerow(row)
+
+    i = i + 1
+    print(str(i) + " write successful!")
